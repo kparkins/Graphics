98f799f3251d36aae7431e36fd3be9a0cf79b60d
Kyle Parkinson <kjp.parkinson@gmail.com>
2015-10-04 12:08:57 -0700

    d

--- Drawable.h
+++ Drawable.h
@@ -1,1 +1,29 @@
-#ifndef DRAWABLE_H#define DRAWABLE_H#include <iostream>#include "Matrix4.h"#include "DrawData.h"#include "UpdateData.h"#include "Material.h"#include <float.h>#include <math.h>#include <vector>class Drawable {    public:        Matrix4 toWorld;    Material material;        Drawable(void);    ~Drawable(void);        virtual void draw(DrawData&);    virtual void update(UpdateData&);    };#endif
+#ifndef DRAWABLE_H
+#define DRAWABLE_H
+
+#include <iostream>
+#include "Matrix4.h"
+#include "DrawData.h"
+#include "UpdateData.h"
+#include "Material.h"
+#include <float.h>
+#include <math.h>
+#include <vector>
+
+
+class Drawable {
+    
+public:
+    
+    Matrix4 toWorld;
+    Material material;
+    
+    Drawable(void);
+    ~Drawable(void);
+    
+    virtual void draw(DrawData&);
+    virtual void update(UpdateData&);
+    
+};
+
+#endif
