51f39091bceec77b3fc20631c93174076f4c92ee
kparkins <kjp.parkinson@gmail.com>
2015-11-12 20:34:46 -0800

    Update math.cpp

--- math.cpp
+++ math.cpp
@@ -3,7 +3,7 @@
 long gfx::math::factorial(unsigned int n) {
     assert(n >= 0);
     long result = 1;
-    for(unsigned int i = 2; i < n; ++i) {
+    for(unsigned int i = 2; i <= n; ++i) {
         result *= i;
     }
     return result;
